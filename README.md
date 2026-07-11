3e9078bc4c3175cad354a7477850879cb54691b5
kondratkoj <220639277+kondratkoj@users.noreply.github.com>
2026-07-11 16:54:34 -0500

    understanding now how webpack should work and modules

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # restaurant_page
 
 This will be my Restaurant Page Project for The Odin Project. We are to use webpack and node to package this finished project. 
+
+I had an idea to make a makeElement function that could cut down what all I need to type when creating dom elements, and added a domHelper.js file to store generic qol functions
--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log("Webpack is working");
+import "./styles/global.css"
+import { makeElement } from "./domHelper.js";
+
+const body = document.querySelector("body");
+const header = makeElement("div", "header");
+const main = makeElement("div", "content");
+const footer = makeElement("div", "footer");
+const pageTitle = makeElement("h1", "pageTitle", "A Humble Restaurant, made with modules");
+const navBar = makeElement("nav", "navBar")
+const homeBtn = makeElement("button", "homeBtn", "Home");
+const menuBtn = makeElement("button", "menuBtn", "Menu");
+const aboutBtn = makeElement("button", "aboutBtn", "About");
+
+body.append(header, main, footer);
+header.append(pageTitle, navBar)
+navBar.append(homeBtn, menuBtn, aboutBtn)
